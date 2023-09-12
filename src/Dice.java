55d2d3dbd92b71fbdd4db4b35daad51db520a12e
rjkiran1619 <rjkiran1619@gmail.com>
2023-09-12 11:43:07 +0530

    latest commit

---
+++ src/Dice.java
@@ -0,0 +1,13 @@
+
+public class Dice {
+int x =4;
+
+	public static void main(String[] args) {
+		Dice myObj = new Dice();
+		System.out.println(myObj.x);
+		
+		// TODO Auto-generated method stub
+        // This is OPPS program for a class and object model
+	}
+
+}
